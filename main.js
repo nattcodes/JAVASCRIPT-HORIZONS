c464b607476b347d0a27a460dc4ba2c8341ea576
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2024-12-02 11:53:30 +0100

    done

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-let Number = 1;
-let numString = '1';
+//declared variable variable name = value;
+let digit1 = 2;
+let digit2 = 5;
+
+console.log(digit1 / digit2)
+
+let userA = "danny"
+let userB = "Jupiter"
+
+console.log(`${userA}`);
+
+//console.log(`text + ${}`)
+//console.log({} && {})
+
+
+
+//read on Data types
